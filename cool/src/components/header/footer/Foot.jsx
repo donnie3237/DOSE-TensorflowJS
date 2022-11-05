bf4883d50cc1218f8f1ba1850ceb606d3a690e86
SoRa <paradorn3237@gmail.com>
2022-11-05 13:32:16 +0700

    .

--- cool/src/components/header/footer/Foot.jsx
+++ cool/src/components/header/footer/Foot.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import './foot.css'
 
 function Foot() {
   return (
-    <div>Foot</div>
+    <div className='foot'>
+      <p>class1:</p>
+      <p>class2:</p>
+    </div>
   )
 }
 
